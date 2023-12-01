d457f71996e93d42b9d383028d5307d7e161fca9
Max Vieweg <max.vieweg@queerreferat-aachen.de>
2023-12-01 12:58:51 +0000

    renamed services

---
+++ startup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# untested
+
+# start proxy first
+cd proxy
+docker compose up -d
+cd ..
+
+# iterate through all submodules and exclude proxy
+for SERVICE in $(git submodule | cut -d " " -f 3 | grep -v proxy)
+do
+    cd $SERVICE
+    docker compose up -d
+    cd ..
+done
