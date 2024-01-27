9a88229ad3b340cd7fc636447723aba68f51f7dd
greedykangaroo01 <mail@christian-koch.eu>
2024-01-27 17:26:17 +0000

    start editing startup script

--- startup.sh
+++ startup.sh
@@ -1,16 +1,33 @@
 #!/bin/bash
-# untested
+# initialize the docker swarm
+docker swarm init
+# JOIN_COMMAND = docker swarm join-token worker
 
-# start proxy first
+# now start the services
+# -> the proxy is the first service to start
 cd proxy
-docker compose up -d
+docker stack deploy -c docker-compose.yml proxy
 cd ..
 
-# iterate through all submodules and exclude proxy
+# -> after the proxy, we can start the order services in arbitrary order
 for SERVICE in $(git submodule | cut -d " " -f 3 | grep -v proxy)
 do
+    # -> cd in to service folder
     cd $SERVICE
-    chmod +x ./startup.sh
-    ./startup.sh
+
+    # -> make the environment variables available
+    if [ -f "./.env" ]; then
+        set -a && . .env && set +a
+    fi
+
+    # -> start the service and join the swarm
+    docker stack deploy -c docker-compose.yml $SERVICE
+
+    # -> if there is an additional startup script, run it
+    if [ -f "./startup.sh" ]; then
+        chmod +x ./startup.sh
+        ./startup.sh
+    fi
+    # -> cd out of the service folder
     cd ..
 done
