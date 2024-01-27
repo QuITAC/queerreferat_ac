7700312534edb845fd69513269312b472c1dc84b
Leon R <leon.rabanus@outlook.de>
2024-01-27 18:14:18 +0000

    Added default list, arg pass and help template to startup

--- startup.sh
+++ startup.sh
@@ -1,24 +1,46 @@
 #!/bin/bash
+# Parse arguments by shifting through them. Currently just used for help.
+while :; do
+    case $1 in
+        -h|-help|--h|--help) showHelp=true           
+        ;;
+        *) break
+    esac
+    shift
+done
+
+if [ $showHelp ]; then
+    echo "HELP"
+    exit 0
+fi
+
+# default services to start without command line argument
+services=("proxy" "authentik" "nextcloud")
+
+# Use command line arguments instead of default if there are variables given
+if [ $# -ne 0 ]; then
+    services=("$@")
+fi
+
 # initialize the docker swarm
-docker swarm init
+# docker swarm init
 # JOIN_COMMAND = docker swarm join-token worker
 
-# now start the services
-# -> the proxy is the first service to start
-cd proxy
-docker stack deploy -c docker-compose.yml proxy
-cd ..
-
 # -> after the proxy, we can start the order services in arbitrary order
-for SERVICE in $(git submodule | cut -d " " -f 3 | grep -v proxy)
+for SERVICE in "${services[@]}"
 do
+    echo "starting $SERVICE";
+
     # -> cd in to service folder
-    cd $SERVICE
+    pushd $SERVICE
 
     # -> make the environment variables available
     if [ -f "./.env" ]; then
         set -a && . .env && set +a
+        echo "in if $PG_PASS2"
     fi
+
+    echo "out of if $PG_PASS"
 
     # -> start the service and join the swarm
     docker stack deploy -c docker-compose.yml $SERVICE
@@ -29,5 +51,5 @@ do
         ./startup.sh
     fi
     # -> cd out of the service folder
-    cd ..
+    popd
 done
